b886ebf9afd89b592e9df76797ce6f4b244e2d66
Gary Page <gary.page.training@outlook.com>
2018-05-03 13:50:45 +0000

    Test commit

--- GSTT.Hack/GSTT.Hack.FrontEnd/Controllers/HomeController.cs
+++ GSTT.Hack/GSTT.Hack.FrontEnd/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace GSTT.Hack.FrontEnd.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Hello, please contact the Product Team for more awesome solutions :)";
 
             return View();
         }
