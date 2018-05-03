7e5778316931d95278a130ec02fb29f4fba3484c
david cunliffe <d_cunliffe_training@hotmail.com>
2018-05-03 15:07:07 +0000

    Adding Document Function

---
+++ GSTT.Hack/GSTT.Hack.Models/Dto/DocumentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSTT.Hack.Models.Dto
+{
+    public class DocumentDto
+    {
+        public string Id { get; set; }
+    }
+}
