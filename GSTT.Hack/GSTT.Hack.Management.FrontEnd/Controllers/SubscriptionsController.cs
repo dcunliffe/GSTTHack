b7b489e2375f1d4f22ff8e41d663ed0a756c70be
Gary Page <gary.page.training@outlook.com>
2018-05-03 16:27:43 +0000

    Scaffold 'Create' View

--- GSTT.Hack/GSTT.Hack.Management.FrontEnd/Controllers/SubscriptionsController.cs
+++ GSTT.Hack/GSTT.Hack.Management.FrontEnd/Controllers/SubscriptionsController.cs
@@ -12,12 +12,19 @@ namespace GSTT.Hack.Management.FrontEnd.Controllers
         // GET: Subscriptions
         public ActionResult Index()
         {
+            var categoryList = new List<CategoryModel>();
+            categoryList.Add(new CategoryModel() { Name = "Cat1", IsSelected = false });
+            categoryList.Add(new CategoryModel() { Name = "Cat2", IsSelected = false });
+            categoryList.Add(new CategoryModel() { Name = "Cat3", IsSelected = false });
+            categoryList.Add(new CategoryModel() { Name = "Cat4", IsSelected = false });
+            categoryList.Add(new CategoryModel() { Name = "Cat5", IsSelected = false });
+
             var dummyModel =
                 new List<SubscriptionsModel>();
             dummyModel.Add(
                 new SubscriptionsModel()
                 {
-                    Categories = new string[] { "Cat1", "Cat2", "Dog1", "Dog2" },
+                    Categories = categoryList,
                     EmailAddress = "Test1234@test.com"
                 });
             return View(dummyModel);
@@ -32,7 +39,18 @@ namespace GSTT.Hack.Management.FrontEnd.Controllers
         // GET: Subscriptions/Create
         public ActionResult Create()
         {
-            return View();
+            var categoryList = new List<CategoryModel>();
+            categoryList.Add(new CategoryModel() { Name = "Cat1", IsSelected = false });
+            categoryList.Add(new CategoryModel() { Name = "Cat2", IsSelected = false });
+            categoryList.Add(new CategoryModel() { Name = "Cat3", IsSelected = false });
+            categoryList.Add(new CategoryModel() { Name = "Cat4", IsSelected = false });
+            categoryList.Add(new CategoryModel() { Name = "Cat5", IsSelected = false });
+
+            var dummyModel = new  SubscriptionsModel()
+                {
+                Categories = categoryList
+                };
+            return View(dummyModel);
         }
 
         // POST: Subscriptions/Create
